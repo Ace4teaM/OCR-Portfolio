3b0d1cd9c7a6dd59eb40a7660912c5a1c0be4fbc
Thomas Auguey <thomas.auguey@hotmail.com>
2026-01-05 13:28:05 +0100

    implémente le formulaire de projets et fix les visuels

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,9 +1,9 @@
-var categories = new Set();
-var works = window.localStorage.getItem("works");
-var token = window.localStorage.getItem("token");
-var noCache = false;
-var api = "http://localhost:5678/api";
-var curCategoryFilter = null;
+let categories = [];
+let works = window.localStorage.getItem("works");
+let token = window.localStorage.getItem("token");
+const noCache = false;
+const api = "http://localhost:5678/api";
+let curCategoryFilter = null;
 
 // Méthode pour récupérer les travaux depuis l'API
 async function getWorks() {
@@ -11,63 +11,86 @@ async function getWorks() {
     works = await reponse.json();
     // Stockage des informations dans le localStorage
     window.localStorage.setItem("works", JSON.stringify(works));
-    console.debug(works);
 }
 
 // Méthode pour obtenir les catégories
 // works doit être initialisé
 function getCategories() {
-    categories.clear();
+    categories = [];
     works.forEach(work => {
-        categories.add(work.category.name);
+        if (!categories.find(x => work.category.id == x.id))
+            categories.push(work.category);
     });
-    console.debug(categories);
+}
+
+// déconnexion de l'utilisateur
+function logout() {
+    window.localStorage.removeItem("token");
+    window.location.reload();
 }
 
 // Evenement pour fermer (cacher) la modale
-const closeButton = document.querySelectorAll(".close-button");
-closeButton.forEach((e) => {
+document.querySelectorAll(".close-button").forEach((e) => {
     e.addEventListener("click", async function (event) {
-        var parentModal = event.target.closest(".modal");
+        const parentModal = event.target.closest(".modal");
         parentModal.classList.add("hidden");
     });
 });
 
+function initPageContent(pageName) {
+    if (pageName === "galery") {
+        // ne fait rien
+    }
+    if (pageName === "upload") {
+        // cache le preview, affiche la sélection, réinitialise le texte
+        const selection = document.querySelector("#upload-file");
+        const preview = document.querySelector("#upload-preview");
+        const message = document.querySelector("#upload-message");
+        const form = document.querySelector("#upload-form");
+        selection.classList.remove("hidden");
+        preview.classList.add("hidden");
+        message.innerText = "";
+        form.reset();
+    }
+}
+
 // Evenement pour afficher la page suivante du modal
-const nextButton = document.querySelectorAll(".next-button");
-nextButton.forEach((e) => {
+document.querySelectorAll(".next-button").forEach((e) => {
     e.addEventListener("click", async function (event) {
-        var parentContent = event.target.closest(".modal-content-page");
+        const parentContent = event.target.closest(".modal-content-page");
         parentContent.classList.add("hidden");
         parentContent.nextElementSibling.classList.remove("hidden");
+        initPageContent(parentContent.nextElementSibling.getAttribute("name"));
     });
 });
 
 // Evenement pour afficher la page précédente du modal
-const prevButton = document.querySelectorAll(".prev-button");
-prevButton.forEach((e) => {
+document.querySelectorAll(".prev-button").forEach((e) => {
     e.addEventListener("click", async function (event) {
-        var parentContent = event.target.closest(".modal-content-page");
+        const parentContent = event.target.closest(".modal-content-page");
         parentContent.classList.add("hidden");
         parentContent.previousElementSibling.classList.remove("hidden");
+        initPageContent(parentContent.previousElementSibling.getAttribute("name"));
     });
 });
 
 // Evenement pour afficher la modale 'gallery'
-const projectEditButton = document.querySelector(".edit-mode-projets a");
-projectEditButton.addEventListener("click", async function (event) {
-    var modal = document.querySelector("#gallery-modal");
+document.querySelector(".edit-mode-projets a").addEventListener("click", async function (event) {
+    const modal = document.querySelector("#gallery-modal");
+    const selection = document.querySelector("#upload-file");
+    const preview = document.querySelector("#upload-preview");
     modal.classList.remove("hidden");
     // affiche la première page, cache les autres
     modal.querySelectorAll(".modal-content-page").forEach(function (content) {
         content.classList.add("hidden");
     });
     modal.querySelector(".modal-content-page:first-child").classList.remove("hidden");
+    // initialise la premiere page
+    initPageContent(modal.querySelector(".modal-content-page").getAttribute("name"));
 });
 
 // Evenement pour fermer (cacher) la modale
-const modalClick = document.querySelectorAll(".modal");
-modalClick.forEach((e) => {
+document.querySelectorAll(".modal").forEach((e) => {
     e.addEventListener("click", async function (event) {
         // s'assure que l'item cliqué n'est pas un enfant du modal
         if (event.target !== e)
@@ -88,13 +111,11 @@ function showEditing() {
 
 // Méthode pour mettre à jour le contenu de la page
 function refreshHtmlWorks() {
-    console.debug("refreshHtmlWorks");
-
     // Actualise la galerie des projets
     const worksContainer = document.querySelector(".gallery");
     worksContainer.innerHTML = "";
     works.forEach(work => {
-        if (curCategoryFilter && work.category.name !== curCategoryFilter)
+        if (curCategoryFilter && work.category.name !== curCategoryFilter.name)
             return;
         const figure = document.createElement("figure");
         figure.innerHTML = `
@@ -135,7 +156,7 @@ function refreshHtmlWorks() {
     filtersContainer.appendChild(firstChild);
     categories.forEach(category => {
         const button = document.createElement("button");
-        button.innerText = category;
+        button.innerText = category.name;
         button.addEventListener("click", function () {
             curCategoryFilter = category;
             refreshHtmlWorks();
@@ -148,8 +169,8 @@ function refreshHtmlWorks() {
     uploadCombo.innerHTML = "";
     categories.forEach(category => {
         const option = document.createElement("option");
-        option.value = category;
-        option.innerText = category;
+        option.value = category.id;
+        option.innerText = category.name;
         uploadCombo.appendChild(option);
     });
 
@@ -159,13 +180,11 @@ function refreshHtmlWorks() {
 }
 
 // affichage du login / déconnection
-const loginLink = document.querySelector("#login-link");
-loginLink.addEventListener("click", async function (event) {
+document.querySelector("#login-link").addEventListener("click", async function (event) {
     // si l'utilisateur est déjà loggé
     if (token) {
         // logout
-        window.localStorage.removeItem("token");
-        window.location.reload();
+        logout();
     }
     // sinon affichage du login
     else {
@@ -179,8 +198,7 @@ loginLink.addEventListener("click", async function (event) {
 });
 
 // affichage du contact
-const contactLink = document.querySelector("#contact-link");
-contactLink.addEventListener("click", async function (event) {
+document.querySelector("#contact-link").addEventListener("click", async function (event) {
     document.querySelectorAll("main>section").forEach(function (element) {
         if (element.id == "contact")
             element.classList.remove("hidden");
@@ -190,8 +208,7 @@ contactLink.addEventListener("click", async function (event) {
 });
 
 // affichage des projets
-const projetsLink = document.querySelector("#projets-link");
-projetsLink.addEventListener("click", async function (event) {
+document.querySelector("#projets-link").addEventListener("click", async function (event) {
     document.querySelectorAll("main>section").forEach(function (element) {
         if (element.id == "login")
             element.classList.add("hidden");
@@ -201,9 +218,9 @@ projetsLink.addEventListener("click", async function (event) {
 });
 
 // formulaire de login
-const loginForm = document.querySelector("#login form");
-loginForm.addEventListener("submit", async function (event) {
+document.querySelector("#login form").addEventListener("submit", async function (event) {
     event.preventDefault(); // Empêche le rechargement de la page
+
     // paramètres de la requête
     const data = {
         email: event.target.querySelector("[name=email]").value,
@@ -217,7 +234,7 @@ loginForm.addEventListener("submit", async function (event) {
     }).then(response => {
         response.json().then(data => {// obtient le corps de la réponse
             console.debug(data);
-            var message = document.querySelector("#login-message");
+            const message = document.querySelector("#login-message");
             if (data.message) {
                 switch (data.message) {
                     case "user not found":
@@ -253,24 +270,124 @@ function deleteWork(id) {
         headers: { "Content-Type": "application/json", "Authorization": "Bearer " + token },
         body: JSON.stringify(data)
     }).then(response => {
-        console.debug(response);
         if (response.ok) {
             // actualise la page
-            getWorks().then(function () { refreshHtmlWorks(); });
+            refreshContent();
         }
     });
 }
+
+// ajoute un projet
+function addWork(data) {
+    // Appel API
+    fetch(api + "/works", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + token },
+        body: JSON.stringify(data)
+    }).then(response => {
+        if (response.ok) {
+            // actualise la page
+            refreshContent();
+        }
+    });
+}
+
+
+// Ajoute un projet via le formulaire de la modale
+document.querySelector("#upload-form").addEventListener("submit", async function (event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    const modal = document.querySelector("#gallery-modal");
+    const form = document.querySelector("#upload-form");
+    const data = new FormData(form);
+    const message = document.querySelector("#upload-message");
+
+    console.debug(data);
+
+    if (data.get("image").size === 0) {
+        message.innerHTML = "Veuillez sélectionner une image.";
+        return;
+    }
+
+    if (data.get("title") === "") {
+        message.innerHTML = "Veuillez saisir un titre.";
+        return;
+    }
+
+    const request = new XMLHttpRequest();
+    request.open("POST", api + "/works", true);
+    request.setRequestHeader('Authorization', "Bearer " + token);
+    request.onload = (progress) => {
+        // Si ok actualise le contenu
+        if (request.status >= 200 && request.status < 300) {
+            // actualise la page
+            modal.classList.add("hidden");
+            refreshContent();
+        }
+        else if (request.status === 401) {
+            alert("Votre session a expiré. Vous devez vous reconnecter.");
+            logout(); // ou refresh token
+        }
+        else {
+            const error = `Erreur ${request.status} lors de la tentative de téléversement du fichier.`;
+            message.innerHTML = error;
+            console.debug(error);
+        }
+    };
+
+    request.send(data);
+});
+
+document.querySelector("#gallery-validate").addEventListener("click", async function (event) {
+    const form = document.querySelector("#upload-form");
+    form.requestSubmit();
+});
+
+// evenement d'upload de l'image
+document.querySelector("#upload-button").addEventListener("click", async function (event) {
+    const selection = document.querySelector("#upload-file");
+    const preview = document.querySelector("#upload-preview");
+    const image = document.querySelector("#upload-image");
+    const input = document.querySelector("#upload-input");
+
+    // (délégue le choix du fichier à l'input natif du formulaire)
+    input.click();
+
+    // après sélectionne met à jour l'aperçu
+    input.addEventListener("change", () => {
+        if (input.files.length === 0) {
+            // L’utilisateur a ouvert la boîte de dialogue puis annulé
+            preview.classList.add("hidden");
+            selection.classList.remove("hidden");
+            return;
+        }
+
+        // L’utilisateur a terminé sa sélection
+        const file = input.files[0];
+        const url = URL.createObjectURL(file);
+        image.src = url;
+        image.onload = () => URL.revokeObjectURL(url); // libère la mémoire
+        preview.classList.remove("hidden");
+        selection.classList.add("hidden");
+    });
+});
 
 
 //****************************************************************** */
 // Programme principal
 //****************************************************************** */
 
+function init() {
+    // Charge les données si besoin et actualise l'affichage
+    if (!works || noCache)
+        getWorks().then(function () { refreshHtmlWorks(); });
+    else {
+        works = JSON.parse(works);
+        refreshHtmlWorks();
+    }
+}
 
-// Charge les données si besoin et actualise l'affichage
-if (!works || noCache)
-    getWorks().then(function () { refreshHtmlWorks(); });
-else {
-    works = JSON.parse(works);
-    refreshHtmlWorks();
+
+async function refreshContent() {
+    await getWorks().then(function () { refreshHtmlWorks(); });
 }
